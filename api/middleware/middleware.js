541f70ffd5f9db0fb452b00452c160300c4dc8fb
ferengitooth <ehitsamill@gmail.com>
2021-09-02 16:41:23 -0700

    fleshed out validate post and hooked it up to .post endpoint

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,3 +1,7 @@
+const User = require('../users/users-model')
+
+
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
   const timestamp = new Date().toLocaleString()
@@ -7,22 +11,48 @@ function logger(req, res, next) {
   next()
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
-  console.log('validateUserId MW')
-  next()
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id)
+    if (!user) {
+      res.status(404).json({
+        message: 'no such user'
+      })
+    } else {
+      req.user = user
+      next()
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'problem finding user'
+    })
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
-  console.log('validateUser MW')
-  next()
+  const { name } = req.body
+  if (!name || !name.trim()) {
+    res.status(400).json({
+      message: 'missing required name field'
+    })
+  } else {
+    req.name = name.trim()
+    next()
+  }
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
-  console.log('validatePost MW')
-  next()
+  const { text } = req.body
+  if (!text || !text.trim()) {
+    res.status(400).json({
+      message: 'missing required text field'
+    })
+  } else {
+    req.text = text.trim()
+    next()
+  }
 }
 
 // do not forget to expose these functions to other modules
